fc065dc4189226a136faa8428179ca416bbd59cd
HARISHWA D REDDY <harishwadreddy@gmail.com>
2024-09-12 14:43:09 +0530

    changed pfp and contact

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -204,15 +204,9 @@ export default function Page() {
                 Get in Touch
               </h2>
               <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                Want to chat? Just shoot me a dm{" "}
-                <Link
-                  href={DATA.contact.social.X.url}
-                  className="text-blue-500 hover:underline"
-                >
-                  with a direct question on twitter
-                </Link>{" "}
-                and I&apos;ll respond whenever I can. I will ignore all
-                soliciting.
+                Want to chat? Feel free to reach out through any of the links in
+                the navbar, and I&apos;ll get back to you as soon as I can. I
+                will ignore all unsolicited messages.
               </p>
             </div>
           </BlurFade>
