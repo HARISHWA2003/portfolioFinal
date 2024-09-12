1b144ce7cde6e585e928755ba315ba60cbfda305
HARISHWA D REDDY <harishwadreddy@gmail.com>
2024-09-12 14:41:08 +0530

    added images and  some changes to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Page() {
                 delay={BLUR_FADE_DELAY}
                 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none"
                 yOffset={8}
-                text={`Hi, I'm ${DATA.name.split(" ")[0]} 👋`}
+                text={`Hi, I'm ${DATA.name.split(" ")[0]}`}
               />
               <BlurFadeText
                 className="max-w-[600px] md:text-xl"
@@ -123,9 +123,9 @@ export default function Page() {
                   Check out my latest work
                 </h2>
                 <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                  I&apos;ve worked on a variety of projects, from simple
-                  websites to complex web applications. Here are a few of my
-                  favorites.
+                  Explore my recent projects in AI, API development, and
+                  full-stack solutions, driving innovation across technology
+                  domains.
                 </p>
               </div>
             </div>
@@ -164,12 +164,10 @@ export default function Page() {
                   I like building things
                 </h2>
                 <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                  During my time in university, I attended{" "}
-                  {DATA.hackathons.length}+ hackathons. People from around the
-                  country would come together and build incredible things in 2-3
-                  days. It was eye-opening to see the endless possibilities
-                  brought to life by a group of motivated and passionate
-                  individuals.
+                  During my time in university, I participated in several
+                  hackathons where teams came together to create innovative
+                  projects in just a few days. It was inspiring to see what
+                  could be achieved with collaboration and creativity.
                 </p>
               </div>
             </div>
